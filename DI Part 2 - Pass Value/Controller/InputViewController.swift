33db51b72f4b1ba549fda49dfc44f4c7b0dd826b
Red Wang <redxoxored@gmail.com>
2023-10-25 22:22:28 +0800

    feat: Add `InputViewController`

--- DI Part 2 - Pass Value/Controller/InputViewController.swift
+++ DI Part 2 - Pass Value/Controller/InputViewController.swift
@@ -5,4 +5,92 @@
 //  Created by Red Wang on 2023/10/25.
 //
 
-import Foundation
+import UIKit
+
+protocol InputViewControllerDelegate: AnyObject {
+    func didChangeNumber(inputNumber: String, cellRow: Int)
+    func didAddNumber(inputNumber: String)
+
+}
+
+enum Action {
+    case change
+    case add
+}
+
+class InputViewController: UIViewController {
+    
+    var delegate: InputViewControllerDelegate?
+    var newNumberHandler: ((String) -> Void)?
+    var action: Action = .add
+    var selectedCellRow: Int?
+    
+    // MARK: - Subviews
+    let numberTextField: UITextField = {
+        let numberTextField = UITextField()
+        numberTextField.textColor = .black
+        numberTextField.font = UIFont.systemFont(ofSize: 14)
+        numberTextField.layer.borderColor = UIColor.darkGray.cgColor
+        numberTextField.layer.borderWidth = 1.0
+        numberTextField.layer.cornerRadius = 5.0
+        return numberTextField
+    }()
+    
+    let enterButton: UIButton = {
+        let enterButton = UIButton()
+        enterButton.setTitle("Enter", for: .normal)
+        enterButton.backgroundColor = .black
+        enterButton.layer.cornerRadius = 5.0
+        return enterButton
+    }()
+    
+    // MARK: - View Load
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+        setUpLayouts()
+        setUpActions()
+        
+    }
+    private func setUpLayouts() {
+        view.addSubview(numberTextField)
+        view.addSubview(enterButton)
+        
+        view.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        
+        NSLayoutConstraint.activate([
+            numberTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            numberTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            numberTextField.heightAnchor.constraint(equalToConstant: 40),
+            numberTextField.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 2.0 / 3.0),
+            
+            enterButton.topAnchor.constraint(equalTo: view.centerYAnchor, constant: 20),
+            enterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            enterButton.heightAnchor.constraint(equalToConstant: 40),
+            enterButton.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width * 2.0 / 3.0)
+        ])
+        
+    }
+    
+    private func setUpActions() {
+        enterButton.addTarget(self, action: #selector(enterButtonPressed), for: .touchUpInside)
+    }
+    
+    // MARK: - Methods
+    @objc func enterButtonPressed() {
+        guard let inputNumber = numberTextField.text,
+              inputNumber != "" else { return }
+        
+        //newNumberHandler?(inputNumber)
+        if action == .add {
+            delegate?.didAddNumber(inputNumber: inputNumber)
+            
+        } else {
+            delegate?.didChangeNumber(inputNumber: inputNumber, cellRow: selectedCellRow!)
+        }
+        navigationController?.popViewController(animated: true)
+    }
+}
